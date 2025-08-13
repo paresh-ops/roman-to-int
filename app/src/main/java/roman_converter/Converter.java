f53ee9e5aa4f6ab599d45fdc91b743a40ed51583
BSTHOSHAN <bsthoshan@gmail.com>
2025-08-12 20:45:35 -0500

    asdasd

--- app/src/main/java/roman_converter/Converter.java
+++ app/src/main/java/roman_converter/Converter.java
@@ -1,4 +1,5 @@
 package roman_converter;
+import java.util.HashMap;
 
 public class Converter {
     public String getGreeting() {
@@ -6,7 +7,24 @@ public class Converter {
     }
 
     public int convert(String roman) {
-        // Your code here!
+        HashMap<char, int> ConverterMap = new HashMap;
+
+        ConverterMap.put('I', 1);
+        ConverterMap.put('V', 5);
+        ConverterMap.put('X', 10);
+        ConverterMap.put('L', 50);
+        ConverterMap.put('C', 100);
+        ConverterMap.put('D', 500);
+        ConverterMap.put('M', 1000);
+
+        private final int result;
+        private final int priviousNum;
+
+
+
+
+        for ( i = )
+
         return 0;
     }
 }
